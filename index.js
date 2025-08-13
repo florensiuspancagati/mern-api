c015314e22a6524db388efbcda2e2001b04d4507
florensiuspancagati <florensiuspancagati@gmail.com>
2025-08-13 13:53:32 +0700

    Fixing CORS origin error

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const express = require('express');
 const app = express();
 const productsRoutes = require('./src/routes/products');
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/', productsRoutes);
 app.listen(3000);
