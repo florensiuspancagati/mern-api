1e0b135bb3ee9ab80a5c04b23186b8a875af64d2
florensiuspancagati <florensiuspancagati@gmail.com>
2025-08-15 20:59:03 +0700

    Add validation using express-validator, Return sample blog post data on successful creation, Handle validation errors with custom error status and message

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,7 +1,17 @@
+const { validationResult } = require('express-validator');
+
 exports.createBlogPost = (req, res, next) => {
     const title = req.body.title;
     // const image = req.body.image;
     const body = req.body.body;
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        const err = new Error('Validation failed, entered data is incorrect.');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    };
 
     const result = {
         message: 'Blog post created successfully',
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 
 const blogController = require('../controllers/blog');
 
 // [post]: /v1/blog/post
-router.post('/post', blogController.createBlogPost);
+router.post('/post',
+    [body('title').isLength({min: 5}).withMessage('Title must be at least 5 characters long'),
+        body('body').isLength({min: 5}).withMessage('Body must be at least 5 characters long')],
+    blogController.createBlogPost);
 
 module.exports = router;
