67bfd379f82bc7c4f37be170269ecf67fd79967b
florensiuspancagati <florensiuspancagati@gmail.com>
2025-08-18 21:05:52 +0700

    make cluster in atlas (mongodb) install mongoose for mongodb Connect to MongoDB (atlas) using mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const authRoutes = require('./src/routes/auth');
@@ -25,4 +26,8 @@ app.use((error, req, res, next) => {
     res.status(status).json({message: message, data: data});
 });
 
-app.listen(3000);
+mongoose.connect('mongodb+srv://panca:mern-api-mongodb-key@cluster0.d7zjvdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        app.listen(3000, () => console.log('Server is running on port 3000'));
+    })
+    .catch(err => console.log(err));
