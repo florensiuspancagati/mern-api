44441861d55e2cd756dbaea35a27489c71c261da
florensiuspancagati <florensiuspancagati@gmail.com>
2025-08-21 13:59:36 +0700

    update  ( put method )

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -65,3 +65,47 @@ exports.getBlogPostById = (req, res, next) => {
     })
     .catch(err => { next(err); });
 }
+
+exports.updateBlogPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        const err = new Error('Validation failed, entered data is incorrect.');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    };
+
+    if(!req.file) {
+        const err = new Error('No image provided.');
+        err.errorStatus = 422;
+        throw err;
+    }
+    
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then(post => {
+        if(!post) {
+            const err = new Error('Could not find blog post.');
+            err.errorStatus = 404;
+            throw err;
+        }
+
+        post.title = title;
+        post.body = body;
+        post.image = image;
+
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Blog post updated successfully',
+            data: result
+        });
+    })
+    .catch(err => { next(err); });
+}
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -10,8 +10,12 @@ router.post('/post',
         body('body').isLength({min: 5}).withMessage('Body must be at least 5 characters long')],
     blogController.createBlogPost);
 
-// [get]: /v1/blog/posts
 router.get('/post/:postId', blogController.getBlogPostById);
 router.get('/posts', blogController.getAllBlogPosts);
+
+router.put('/post/:postId',
+    [body('title').isLength({min: 5}).withMessage('Title must be at least 5 characters long'),
+        body('body').isLength({min: 5}).withMessage('Body must be at least 5 characters long')],
+    blogController.updateBlogPost);
 
 module.exports = router;
